76f8844f0489c7e178e13c05df4db1d1d47432b4
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-22 12:09:56 +0300

    Style for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='calculator-grid'>
+        <div id='display'>
+          <div className='previous-operand'></div>
+          <div className='current-operand'></div>
+        </div>
+        <button id='clear'>AC</button>
+        <button id='divide'>/</button>
+        <button id='multiply'>x</button>
+        <button id='one'>1</button>
+        <button id='two'>2</button>
+        <button id='three'>3</button>
+        <button id='subtract'>-</button>
+        <button id='four'>4</button>
+        <button id='five'>5</button>
+        <button id='six'>6</button>
+        <button id='add'>+</button>
+        <button id='seven'>7</button>
+        <button id='eight'>8</button>
+        <button id='nine'>9</button>
+        <button id='decimal'>.</button>
+        <button id='zero'>0</button>
+        <button id='equals'>=</button>
+      </div>
+    </>
   );
 }
 
